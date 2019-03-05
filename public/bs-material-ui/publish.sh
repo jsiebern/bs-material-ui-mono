0df00d922f81fb28749e85e89c2bf4a7fa8af402
Jonathan Siebern <jsiebern88@gmail.com>
2019-03-05 16:17:46 +0100

    -

--- public/bs-material-ui/publish.sh
+++ public/bs-material-ui/publish.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# "prepublish": "mv ./bsconfig.json ./bsc.json; mv ./bsconfig-npm.json ./bsconfig.json",
-# "postpublish": "mv ./bsconfig.json ./bsconfig-npm.json; mv ./bsc.json ./bsconfig.json"
+mv ./bsconfig.json ./bsc.json; mv ./bsconfig-npm.json ./bsconfig.json
+yarn publish
+mv ./bsconfig.json ./bsconfig-npm.json; mv ./bsc.json ./bsconfig.json
